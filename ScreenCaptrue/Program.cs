07acc8973dbe889809e6b22b118972caffdb0f3a
Richy <timemonsters@163.com>
2025-03-21 14:51:42 +0800

    删除

--- ScreenCaptrue/Program.cs
+++ ScreenCaptrue/Program.cs
@@ -14,6 +14,12 @@ namespace ScreenCaptrue
         [STAThread]
         static void Main()
         {
+            Application.ThreadException += (sender, e) =>
+            MessageBox.Show($"未处理异常: {e.Exception.ToString()}");
+            Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+
+
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form2());
